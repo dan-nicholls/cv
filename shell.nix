f8824ea7fb3b300357c5d02c7333d9661d9681b6
Dan Nicholls <dannicholls12@gmail.com>
2025-05-06 21:10:13 +1000

    chore: add build alias to nix-shell

--- shell.nix
+++ shell.nix
@@ -4,4 +4,8 @@ pkgs.mkShell {
 	buildInputs = with pkgs; [
 		texlive.combined.scheme-full
 	];
+
+	shellHook = ''
+		alias buildcv='xelatex -interaction=nonstopmode -output-directory=build cv.tex'
+	'';
 }
