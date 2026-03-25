92c8ce50e793b3b63812817410b5e0e19b24fd49
Dan Nicholls <dannicholls12@gmail.com>
2026-03-25 18:32:23 +1100

    feat: add support for video cover letter

--- shell.nix
+++ shell.nix
@@ -1,12 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+{pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
-	buildInputs = with pkgs; [
-		texlive.combined.scheme-full
-	];
+  buildInputs = with pkgs; [
+    texlive.combined.scheme-full
+    curl
+  ];
 
-		shellHook = ''
-			alias buildcv='xelatex -interaction=nonstopmode -output-directory=build cv.tex'
-			alias buildcover='xelatex -interaction=nonstopmode -output-directory=build cover_letter.tex'
-		'';
+  shellHook = ''
+    alias buildcv='xelatex -interaction=nonstopmode -output-directory=build cv.tex'
+    alias buildcover='xelatex -interaction=nonstopmode -output-directory=build cover_letter.tex'
+    alias buildvideocover='xelatex -shell-escape -interaction=nonstopmode -output-directory=build video_cover_letter.tex'
+  '';
 }
